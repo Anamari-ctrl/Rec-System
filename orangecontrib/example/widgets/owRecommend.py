89ca740d6d73ff1e7e30133818f08d4bca14241e
Anamari-ctrl <ao6477@student.uni-lj.si>
2024-02-12 14:25:34 +0100

    add label with names of the neighbours

--- orangecontrib/example/widgets/owRecommend.py
+++ orangecontrib/example/widgets/owRecommend.py
@@ -3,9 +3,10 @@ import numpy as np
 import Orange
 from Orange.data import Table
 from Orange.widgets.widget import OWWidget, Input, Output
-from Orange.widgets import gui
+from Orange.widgets import gui, settings
 from orangecontrib.network import Network
 from orangewidget.settings import Setting
+from orangewidget.utils.itemmodels import PyListModel
 
 
 class Recommendation(OWWidget):
@@ -14,21 +15,25 @@ class Recommendation(OWWidget):
     icon = "icons/recommendation.png"
     category = "Recommendation"
 
-
     class Inputs:
         network = Input("Network", Network, default=True)
 
-
-    kid = Setting(0)
+    kid = settings.ContextSetting(None)
     explanation = Setting(0)
 
     def __init__(self):
         super().__init__()
-        self.network = None
+        self.network: Network  = None
+        self.cartoons_list_label = None
+        self.friends_list_label = None
 
-        gui.comboBox(self.controlArea, self, "kid", box="Kid")
-        gui.label(self.controlArea, self, label="Cartoons list", box="Cartoons", orientation="horizontal")
-        gui.label(self.controlArea, self, label="Friends list", box="Friends", orientation="horizontal")
+        self.nodes_model = PyListModel()
+        value_combo = gui.comboBox(
+            self.controlArea, self, "kid", label="Kid: ",
+            model=self.nodes_model, contentsLength=10, callback=self.set_friends)
+
+        gui.label(self.controlArea, self, label="Cartoons asdf", box="Cartoons", orientation="horizontal")
+        self.friends_list_label =gui.label(self.controlArea, self, label="Friends list", box="Friends", orientation="horizontal")
 
         gui.listView(self.mainArea, self, "all_recommendations", box="Recommendations")
         gui.label(self.mainArea, self, "here we give explanations", box="Explanation", )
@@ -36,7 +41,18 @@ class Recommendation(OWWidget):
     @Inputs.network
     def set_network(self, network):
         self.network = network
+        self.set_value_list()
+        self.set_friends()
 
+    def set_value_list(self):
+        self.nodes_model[:] = self.network.nodes
+    def set_friends(self):
+        idx = np.where(self.network.nodes == self.kid)[0]
+        if len(idx) == 0:
+            return
+        neighbours = self.network.neighbours(idx[0])
+        neighbours_names = [self.network.nodes[i] for i in neighbours]
+        self.friends_list_label.setText("\n".join(neighbours_names))
 
 
 def main():
