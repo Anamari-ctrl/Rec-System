25880eee841c60c606459ce57b1ba9c852f1e247
Anamari-ctrl <ao6477@student.uni-lj.si>
2024-02-12 11:22:26 +0100

    corrected inputs

--- orangecontrib/example/widgets/owRecommend.py
+++ orangecontrib/example/widgets/owRecommend.py
@@ -14,44 +14,29 @@ class Recommendation(OWWidget):
     icon = "icons/recommendation.png"
     category = "Recommendation"
 
+
     class Inputs:
-        node_data = Input("Node Data", Table)
-        node_subset = Input("Node Subset", Table)
         network = Input("Network", Network, default=True)
-        node_distances = Input("Node Distances", Orange.misc.DistMatrix)
+
 
     kid = Setting(0)
     explanation = Setting(0)
 
     def __init__(self):
         super().__init__()
-        self.node_data = None
-        self.node_subset = None
         self.network = None
-        self.node_distances = None
 
-        gui.comboBox(self.controlArea, self, "kid", box="Kid", items=Table.get_column(self.node_data, 0))
+        gui.comboBox(self.controlArea, self, "kid", box="Kid")
         gui.label(self.controlArea, self, label="Cartoons list", box="Cartoons", orientation="horizontal")
         gui.label(self.controlArea, self, label="Friends list", box="Friends", orientation="horizontal")
 
         gui.listView(self.mainArea, self, "all_recommendations", box="Recommendations")
         gui.label(self.mainArea, self, "here we give explanations", box="Explanation", )
 
-    @Inputs.node_data
-    def set_node_data(self, node_data):
-        self.node_data = node_data
-
-    @Inputs.node_subset
-    def set_node_subset(self, node_subset):
-        self.node_subset = node_subset
-
     @Inputs.network
     def set_network(self, network):
         self.network = network
 
-    @Inputs.node_distances
-    def set_node_distances(self, node_distances):
-        self.node_distances = node_distances
 
 
 def main():
