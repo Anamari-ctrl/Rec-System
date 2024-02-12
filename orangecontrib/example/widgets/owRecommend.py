268189f300947959ffce70759c77728f52e26bcc
Anamari-ctrl <ao6477@student.uni-lj.si>
2024-02-12 08:44:29 +0100

    defined inputs

--- orangecontrib/example/widgets/owRecommend.py
+++ orangecontrib/example/widgets/owRecommend.py
@@ -1,9 +1,11 @@
 import numpy as np
+
+import Orange
+from Orange.data import Table
 from Orange.widgets.widget import OWWidget, Input, Output
 from Orange.widgets import gui
 from orangecontrib.network import Network
 from orangewidget.settings import Setting
-import numpy as np
 
 
 class Recommendation(OWWidget):
@@ -13,34 +15,54 @@ class Recommendation(OWWidget):
     category = "Recommendation"
 
     class Inputs:
-        image = Input("Network", Network)
+        node_data = Input("Node Data", Table)
+        node_subset = Input("Node Subset", Table)
+        network = Input("Network", Network, default=True)
+        node_distances = Input("Node Distances", Orange.misc.DistMatrix)
 
     kid = Setting(0)
     explanation = Setting(0)
+
     def __init__(self):
         super().__init__()
+        self.node_data = None
+        self.node_subset = None
+        self.network = None
+        self.node_distances = None
 
-        gui.comboBox(self.controlArea, self, "kid", box="Kid",
-                     items=["kid1", "kid2", "kid3", "kid4", "kid5", "kid6", "kid7", "kid8", "kid9", "kid10"],
-                     callback=self.update_recommendation)
+        gui.comboBox(self.controlArea, self, "kid", box="Kid", items=Table.get_column(self.node_data, 0))
         gui.label(self.controlArea, self, label="Cartoons list", box="Cartoons", orientation="horizontal")
         gui.label(self.controlArea, self, label="Friends list", box="Friends", orientation="horizontal")
 
-        gui.listView(self.mainArea, self, "all_recommendations", box="Recommendations", callback=self.update_recommendation)
-        gui.label(self.mainArea, self, "here we give explanations", box="Explanation",)
+        gui.listView(self.mainArea, self, "all_recommendations", box="Recommendations")
+        gui.label(self.mainArea, self, "here we give explanations", box="Explanation", )
+
+    @Inputs.node_data
+    def set_node_data(self, node_data):
+        self.node_data = node_data
+
+    @Inputs.node_subset
+    def set_node_subset(self, node_subset):
+        self.node_subset = node_subset
+
+    @Inputs.network
+    def set_network(self, network):
+        self.network = network
+
+    @Inputs.node_distances
+    def set_node_distances(self, node_distances):
+        self.node_distances = node_distances
 
 
-    def all_kids(self):
-        return "abc"
-        # lines = open("cartoons_table/kids_cartoons.csv").readlines()
-        # self.kids = np.array([row.split(';')[0] for row in lines[1:]])
+def main():
+    from Orange.widgets.utils.widgetpreview import WidgetPreview
+    from orangecontrib.network.network.readwrite \
+        import read_pajek
+    from os.path import join, dirname
 
-
-    def update_recommendation(self):
-        return "abc"
+    network = read_pajek(join(dirname(dirname(__file__)), 'networks', 'kids_cartoons.net'))
+    WidgetPreview(Recommendation).run(set_network=network)
 
 
 if __name__ == "__main__":
-    from Orange.widgets.utils.widgetpreview import WidgetPreview
-    WidgetPreview(Recommendation).run()
-
+    main()
