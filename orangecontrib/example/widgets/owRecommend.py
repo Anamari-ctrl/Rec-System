275303a19f03629eb48061b6dd1512c83858cc09
Anamari-ctrl <ao6477@student.uni-lj.si>
2024-02-14 13:02:20 +0100

    add friends and cartoons for a node

--- orangecontrib/example/widgets/owRecommend.py
+++ orangecontrib/example/widgets/owRecommend.py
@@ -1,16 +1,22 @@
 import numpy as np
 
+from AnyQt.QtCore import Qt, QSize
+from AnyQt.QtGui import QFontMetrics
+
 import Orange
 from Orange.data import Table
 from Orange.widgets.widget import OWWidget, Input, Output
 from Orange.widgets import gui, settings
+from Orange.widgets.utils.itemmodels import DomainModel
+
 from orangecontrib.network import Network
+
 from orangewidget.settings import Setting
 from orangewidget.utils.itemmodels import PyListModel
 
 
 class Recommendation(OWWidget):
-    name = "Recommendation System 1"
+    name = "Recommendation System 👍"
     description = "Recommend cartoons for kids"
     icon = "icons/recommendation.png"
     category = "Recommendation"
@@ -20,6 +26,10 @@ class Recommendation(OWWidget):
 
     kid = settings.ContextSetting(None)
     explanation = Setting(0)
+    node_name = settings.ContextSetting(None)
+    want_control_area = False
+
+    resizing_enabled = False
 
     def __init__(self):
         super().__init__()
@@ -27,45 +37,118 @@ class Recommendation(OWWidget):
         self.cartoons_list_label = None
         self.friends_list_label = None
 
+        box = gui.hBox(self.mainArea)
+        box2 = gui.vBox(box, "👧🏽🧒🏽Node")
+        self.node_name_model = DomainModel(valid_types=Orange.data.StringVariable)
+        gui.comboBox(
+            box2, self, "node_name", label="Name column: ",
+            model=self.node_name_model, callback=self.set_value_list, orientation=Qt.Horizontal)
+
         self.nodes_model = PyListModel()
-        value_combo = gui.comboBox(
-            self.controlArea, self, "kid", label="Kid: ",
-            model=self.nodes_model, contentsLength=10, callback=self.set_friends)
+        gui.comboBox(
+            box2, self, "kid", label="Name ",
+            model=self.nodes_model, contentsLength=10, callback=self.set_friends, orientation=Qt.Horizontal)
+        gui.rubber(box2)
+        fm = QFontMetrics(self.font())
+        size = QSize(40 * fm.averageCharWidth(), 5 * fm.height())
+        self.cartoons_list_label = gui.label(box, self, label="cartoons", box="Cartoons🦆🪿", minimumSize=size)
+        self.friends_list_label = gui.label(self.mainArea, self, label="Friends list", box="Friends🤹🏼‍♀️🤹🏼",
+                                            minimumSize=size)
 
-        self.cartoons_list_label = gui.label(self.controlArea, self, label="cartoons", box="Cartoons: ",
-                                             orientation="horizontal")
-        self.friends_list_label = gui.label(self.controlArea, self, label="Friends list", box="Friends",
-                                            orientation="horizontal")
-
-        gui.listView(self.mainArea, self, "all_recommendations", box="Recommendations")
-        gui.label(self.mainArea, self, "here we give explanations", box="Explanation", )
+        box3 = gui.hBox(self.mainArea, "Recommendations🏆")
+        self.rec = gui.widgetLabel(box3, minimumSize=QSize(40 * fm.averageCharWidth(), 5 * fm.height()))
 
     @Inputs.network
     def set_network(self, network):
         self.network = network
+        self.node_name_model.set_domain(network.nodes.domain)
+
+
+        if self.node_name_model:
+            self.node_name = self.node_name_model[0]
         self.set_value_list()
         self.set_friends()
+        self.set_cartoons()
 
     def set_value_list(self):
-        self.nodes_model[:] = self.network.nodes
+        if self.node_name is None:
+            self.nodes_model.clear()
+        else:
+            self.nodes_model[:] = self.network.nodes.get_column(self.node_name)
 
     def set_friends(self):
-        idx = np.where(self.network.nodes == self.kid)[0]
+        if self.node_name is None:
+            self.friends_list_label.setText("No friends")
+            return
+        names = self.network.nodes.get_column(self.node_name)
+        idx = np.where(names == self.kid)[0]
         if len(idx) == 0:
             return
         neighbours = self.network.neighbours(idx[0])
-        neighbours_names = [self.network.nodes[i] for i in neighbours]
-        print("neighbours names: ", neighbours_names)
+        neighbours_names = names[neighbours]
+        # print("neighbours names: ", neighbours_names)
+
+        # self.friends_list_label.setText("<ul style='font-size: 12px; list-style-type: lower-roman;'>" + "".join([
+        # "<li>" + name + "</li>" for name in neighbours_names]) + "</ul>")
+        self.friends_list_label.setText(
+            "<ul style='font-size: 12px; list-style-type: none;'>" +
+            "".join(["<li>🍀 " + name + "</li>" for name in neighbours_names]) +
+            "</ul>"
+        )
         self.set_cartoons()
-        self.friends_list_label.setText("\n".join(neighbours_names))
+
+        # print("node_name", self.kid)
+        # print("nodes", self.network.nodes)
+        # print("nodes type", type(self.network.nodes))
+        # print("nodes domain", self.network.nodes.domain)
+        # get row names - cartoons from network nodes
 
     def set_cartoons(self):
-        idx = np.where(self.network.nodes == self.kid)[0]
-        cartoons_str, kid_str = (self.network.nodes[idx[0]]).split('{')
-        cartoons_list = eval(cartoons_str)
-        cartoons_index = [i for i in range(len(cartoons_list)) if cartoons_list[i] == 1]
-        cartoon_names = ['cartoon ' + str(i) for i in cartoons_index]
-        self.cartoons_list_label.setText("\n".join(cartoon_names))
+        names = self.network.nodes.get_column(self.node_name)
+        id_list = np.where(names == self.kid)[0]
+        if len(id_list) == 0:
+            return
+        idx = id_list[0]
+
+        if self.network.nodes.domain is None:
+            self.cartoons_list_label.setText("No cartoons")
+        else:
+
+            node_string = str(self.network.nodes[idx])
+            node_list = eval(node_string[:node_string.find('{')])
+            node_set = eval(node_string[node_string.find('{'):].replace("{", "{'").replace("}", "'}"))
+
+            if self.kid in node_set:
+                cartoons_names = [self.network.nodes.domain[i].name for i in range(len(node_list)) if node_list[i] == 1]
+
+                self.cartoons_list_label.setText(
+                    "<ul style='font-size: 12px; list-style-type: none;'>" +
+                    "".join(["<li>🎭 " + cartoon + "</li>" for cartoon in cartoons_names]) +
+                    "</ul>"
+                )
+        self.recommendations()
+
+    def recommendations(self):
+        names = self.network.nodes.get_column(self.node_name)
+        id_list = np.where(names == self.kid)[0]
+        if len(id_list) == 0:
+            return
+        idx = id_list[0]
+
+        node_string = str(self.network.nodes[idx])
+        node_list = eval(node_string[:node_string.find('{')])
+        cartoons_names = [self.network.nodes.domain[i].name for i in range(len(node_list)) if node_list[i] == 1]
+
+
+        neighbours = self.network.neighbours(idx)
+        print(cartoons_names)
+        print(neighbours)
+        # Find cartoons that the neighbors have watched but the selected kid has not watched yet
+        for neighbour in neighbours:
+            if neighbour == idx:
+                continue
+            else:
+                neighbour_cartoons =
 
 
 
@@ -76,7 +159,9 @@ def main():
         import read_pajek
     from os.path import join, dirname
 
-    network = read_pajek(join(dirname(dirname(__file__)), 'networks', 'kids_cartoons_new.net'))
+    table = Table(join(dirname(dirname(__file__)), 'networks', 'kids_cartoons_new.xlsx'))
+    network = read_pajek(join(dirname(dirname(__file__)), 'networks', 'kids_cartoons_3.net'))
+    network.nodes = table
     WidgetPreview(Recommendation).run(set_network=network)
 
 
